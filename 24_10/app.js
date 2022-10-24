696d8124bc83ff595674f9ba18119d3e6d1392db
Beksultan <zxc.beksultan@gmail.com>
2022-10-24 10:30:59 +0100

    init: added 24_10

---
+++ 24_10/app.js
@@ -0,0 +1,15 @@
+let name = 'Beksultan';
+let sayHi = `Привет, ${name}`;
+
+alert(sayHi);
+
+let age = 22;
+let sayAge = `Мне ${age} лет!`;
+
+alert(sayAge);
+
+let str1 = 'САЛАМ';
+let str2 = 'БИШКЕК!'
+let sayStr = `${str1}, ${str2}`;
+
+alert(sayStr);
