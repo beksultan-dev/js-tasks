7548f9cf2cc930a0de02513fb348a35497ebeced
Beksultan <zxc.beksultan@gmail.com>
2022-10-25 10:51:56 +0100

    init: added 25_10

---
+++ 25_10/app.js
@@ -0,0 +1,28 @@
+/****** 1 *******/
+let a = 17;
+let b = 10;
+let c = a - b;
+let d = 7;
+let result = c + d;
+console.log(result);
+
+/****** 2 *******/
+let hour, minute, second;
+hour = 10;
+minute = 32;
+second = 50;
+let currentTime = `${hour}:${minute}:${second}`;
+console.log(currentTime);
+
+/****** 3 *******/
+/*
+var num = 10;
+num = num + 1;
+num = num + 1;
+alert(num);
+*/
+
+let num = 10;
+num = ++num;
+num = ++num;
+alert(num);
