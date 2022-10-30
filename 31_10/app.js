f87d0479de5bef81829997274fcf2cb30ed60264
Beksultan <zxc.beksultan@gmail.com>
2022-10-30 10:34:45 +0000

    init: added 31_10

---
+++ 31_10/app.js
@@ -0,0 +1,36 @@
+// ********** task 1 *************
+const userSaleries = {};
+
+userSaleries.Ella = 420;
+userSaleries.Sophie = 500;
+userSaleries.Ellie = 250;
+
+let ella = userSaleries.Ella;
+let ellie = userSaleries.Ellie;
+
+console.log(`1) Зарплата Ella:${ella}, Зарплата Ellie:${ellie}`);
+
+
+// ********** task 2 *************
+const numbers = {
+	a: 5,
+	b: 6,
+	c: 10,
+}
+
+let numbersSum = numbers.a + numbers.b + numbers.c;
+console.log(`2) Суммы:${numbersSum}`);
+
+
+// ********** task 3 *************
+// let number;
+// if(data === true){
+//    number = 3;
+// } else{
+//    number = 5;
+// }
+
+let number;
+let data;
+
+number = data === true ? number = 3 : number = 5;
