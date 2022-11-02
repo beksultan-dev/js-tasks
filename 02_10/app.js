92d0ac87f8e8ad1fcdd19e7d13029266f1f6f66e
Beksultan <zxc.beksultan@gmail.com>
2022-11-02 10:46:02 +0000

    init: added 02_10

---
+++ 02_10/app.js
@@ -0,0 +1,22 @@
+// ************** task 1 ****************
+function sayHello() {
+	return 'Привет, JavaScript';
+}
+const result = sayHello();
+console.log(result);
+
+
+// ************** task 2 ****************
+function showPow(number) {
+	console.log(number ** 2);
+}
+showPow(3);
+
+
+// ************** task 3 ****************
+const userName = 'Василий';
+
+function getName(user = 'Привет, гость') {
+	console.log(`Привет, ${user}`);
+}
+getName(userName);
