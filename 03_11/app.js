a7c6a10cb5295d1c55acfe1bf639a7b156bd0f34
Beksultan <zxc.beksultan@gmail.com>
2022-11-03 18:13:54 +0000

    fix: code optimized

--- 03_11/app.js
+++ 03_11/app.js
@@ -12,27 +12,23 @@ const myFunc = (elem, count) => {
 	for (let i = 0; i < count; i++) {
 		arr.push(elem);
 	}
-	console.log(`2. Новый массив: ${arr}`);
-}
 
-myFunc('func', 5);
+	console.log(`2. Новый массив: ${arr}`);
+	return arr;
+}
+const newArr1 = myFunc('func', 5);
 
 
 // *********** task 3 ************
-const thirdArr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const thirdArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 const reverseArr = (array) => {
-	let arr = [];
-
-	// for (let i = -1; i >= -(array.length); i--) {
-	// 	arr.push(array.at(i));
-	// }
-	// console.log(`3. Обратный массив: ${arr}`);
-
+	const arr = [];
 	for (let i = 1; i <= array.length; i++) {
 		arr.push(array.at(-i));
 	}
-	console.log(`3. Обратный массив: ${arr}`);
-}
 
-reverseArr(thirdArr);
+	console.log(`3. Обратный массив: ${arr}`);
+	return arr;
+}
+const newArr2 = reverseArr(thirdArr);
