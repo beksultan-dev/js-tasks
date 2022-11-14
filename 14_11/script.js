2e804b8106f1e9cbd313dc9ab788ba234f510140
Beksultan <zxc.beksultan@gmail.com>
2022-11-14 19:43:50 +0000

    fix: code optimized

--- 14_11/script.js
+++ 14_11/script.js
@@ -1,36 +1,27 @@
-// 1) Напишите функцию showNotification(options), которая создаёт уведомление:
-// <div class="notification"> с заданным содержимым.
-// Уведомление должно автоматически исчезнуть через 1,5 секунды.
-
-// Пример объекта options:
-// ? показывает элемент с текстом "Hello" рядом с правой верхней частью окна.
-// showNotification({
-// html: "Hello!", // HTML-уведомление
-// className: "welcome" // дополнительный класс для div (необязательно)
-// });
-
 // ************* task ***************
-const btns = document.querySelectorAll('.btn');
-const notifications = document.querySelector('.notification');
+const buttons = document.querySelectorAll('.btn');
+const notification = document.querySelector('.notification');
 
 const options = {
 	class: 'welcome',
 	text: 'Уведомление',
 }
 
-const deleteNotification = () => {
-	notifications.classList.remove(options.class);
+const showNotification = (obj) => { // принимает обьект 'options'
+	notification.classList.add(obj.class); // добавляет класс к элементу 'notification'
+	notification.textContent = obj.text; // добавляет текст к элементу 'notification'
+
+	const deleteNotification = () => {
+		notification.classList.remove(obj.class); // удаляет класс у элемента 'notification'
+	}
+	setTimeout(deleteNotification, 1500); // вызывает функцию удаления класса через 1,5 сек
 }
 
-const showNotification = (object) => {
-	notifications.classList.add(object.class);
-	notifications.textContent = object.text;
-
-	setTimeout(deleteNotification, 1500);
+const checkButtons = (collection) => { // параметр принимает коллекцию кнопок 'buttons'
+	for (let i = 0; i < collection.length; i++) { // перебирает каждый элемент коллекции
+		collection[i].addEventListener('click', () => { // добавляет каждому элементу addEventListener
+			showNotification(options); // вызов главной функции 'showNotification'
+		})
+	}
 }
-
-for (let i = 0; i < btns.length; i++) {
-	btns[i].addEventListener('click', () => {
-		showNotification(options);
-	})
-}
+checkButtons(buttons); // передает коллекцию кнопок как аргумент
