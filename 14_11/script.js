5be84dda84854840aa0fd24e7fe436511957272c
Beksultan <zxc.beksultan@gmail.com>
2022-11-14 19:46:23 +0000

    fix: code optimized

--- 14_11/script.js
+++ 14_11/script.js
@@ -8,20 +8,20 @@ const options = {
 }
 
 const showNotification = (obj) => { // принимает обьект 'options'
-	notification.classList.add(obj.class); // добавляет класс к элементу 'notification'
-	notification.textContent = obj.text; // добавляет текст к элементу 'notification'
+	notification.classList.add(obj.class);
+	notification.textContent = obj.text;
 
 	const deleteNotification = () => {
-		notification.classList.remove(obj.class); // удаляет класс у элемента 'notification'
+		notification.classList.remove(obj.class);
 	}
-	setTimeout(deleteNotification, 1500); // вызывает функцию удаления класса через 1,5 сек
+	setTimeout(deleteNotification, 1500);
 }
 
 const checkButtons = (collection) => { // параметр принимает коллекцию кнопок 'buttons'
-	for (let i = 0; i < collection.length; i++) { // перебирает каждый элемент коллекции
-		collection[i].addEventListener('click', () => { // добавляет каждому элементу addEventListener
-			showNotification(options); // вызов главной функции 'showNotification'
+	for (let i = 0; i < collection.length; i++) {
+		collection[i].addEventListener('click', () => {
+			showNotification(options);
 		})
 	}
 }
-checkButtons(buttons); // передает коллекцию кнопок как аргумент
+checkButtons(buttons);
